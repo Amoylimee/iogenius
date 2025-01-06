6526d6c92f5754a6f8e7d3907308209f6782dff9
Jiajing <jchenhl@connect.ust.hk>
2025-01-06 14:56:23 +0800

    Replace tqdm with rich.track which is beautuful

--- src/iogenius/__init__.py
+++ src/iogenius/__init__.py
@@ -2,6 +2,8 @@
 
 from .functions import (
     set_working_directory,
+    create_new_directory,
+    concat_files_in_folder
     # 其他函数名...
 )
 
@@ -9,5 +11,7 @@ __version__ = "0.1.0"
 
 __all__ = [
     "set_working_directory",
+    "create_new_directory",
+    "concat_files_in_folder"
     # 其他函数名...
 ]
--- src/iogenius/functions.py
+++ src/iogenius/functions.py
@@ -1,4 +1,9 @@
 import os
+import glob
+import gc
+import pandas as pd
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from rich.progress import track
 
 def set_working_directory(working_directory: str) -> None:
     """
@@ -7,3 +12,45 @@ def set_working_directory(working_directory: str) -> None:
     os.chdir(working_directory) 
     
     return print(f'Working directory set to {working_directory}')
+
+
+def create_new_directory(directory_path: str) -> None:
+    """Create a new folder/directory if the folder/directory does not exist.
+
+    io_create_new_folder(/disk/r046/jchenhl/Project_RBF/)
+    """
+    if os.path.isdir(directory_path):
+        pass
+    else:
+        os.makedirs(directory_path)
+    return
+
+
+def read_and_merge(file, format = 'feather'):
+
+    if format == 'feather':
+        return pd.read_feather(file)  # 使用 feather 格式读取
+    elif format == 'parquet':
+        return pd.read_parquet(file)
+    elif format == 'csv':
+        return pd.read_csv(file)
+
+
+def concat_files_in_folder(directory_in: str, format = 'feather', max_workers=24) -> pd.DataFrame:
+    print(f"Concatenating files in {directory_in}...")
+    files_in = glob.glob(f"{directory_in}/*{format}")
+    dataframes = []
+    
+    with ProcessPoolExecutor(max_workers=max_workers) as executor:
+        futures = {executor.submit(read_and_merge, file, format): file for file in files_in}
+        for future in track(as_completed(futures), total=len(futures), description="Concatenating..."):
+            try:
+                data = future.result()
+                dataframes.append(data)
+            except Exception as e:
+                print(f"Error reading file: file {futures[future]}: {e}")
+                print(str(e))
+                
+    df = pd.concat(dataframes, axis=0)
+    gc.collect()
+    return df
